8faf832c308d1a040f51cc200d0a593659bb538c
samsonmeseret <samsonmeseretb@gmail.com>
2022-06-08 20:10:29 +0300

    little updates

---
+++ Nav-Hero/src/App.js
@@ -0,0 +1,14 @@
+import Hero from "./components/hero";
+import Navigation from "./components/navigation";
+import { useState } from "react";
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+    </>
+  );
+};
+
+export default App;
---
+++ Nav-Hero/src/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    name: "Samson Meseret",
+    age: 26,
+  },
+  {
+    id: 2,
+    name: "Alazar Tegene",
+    age: 25,
+  },
+  {
+    id: 3,
+    name: "Rediat Zelalem",
+    age: 28,
+  },
+];
